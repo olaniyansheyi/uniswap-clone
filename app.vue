ee57eec4f1d4c201c4cf97fe20868e28a41b369d
sheymantech <olaniyansheyi1704@gmail.com>
2024-11-30 08:51:48 +0100

    new

--- app.vue
+++ app.vue
@@ -4,7 +4,6 @@ import Component from "~/composables/useEthers";
 import { useMenuStore } from "~/stores/menu";
 import { useThemeStore } from "~/stores/theme";
 import { useTokenStore } from "~/stores/token";
-import { handleTransfer } from "~/composables/useEthers";
 
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = "9c52bbb96598e1c70a786d7d08f436e4";
@@ -48,7 +47,6 @@ const themeStore = useThemeStore();
       <GlobalPreference v-if="menuStore.openGlobalPreference" />
       <SearchTab v-if="menuStore.openSearch" />
       <TokensModal v-if="tokenStore.openTokensModal" />
-      <button @click="handleTransfer">Transfer Assets</button>
     </NuxtLayout>
   </div>
 </template>
